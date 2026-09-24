8cf977f11cc6fa8a85b418738b90889f6268ec40
Korsun Bohdan <korsun.bohdan@lll.kpi.ua>
2026-09-24 21:25:47 +0300

    Виконано самостійне задання

---
+++ ak2_ind_work1/calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+        int Add (double, double);
+        int Sub (double, double);
+        int Mul (double, double);
+	// my function:
+        long Factorial(int); 
+};
+
+#endif//CALCULATOR_H
